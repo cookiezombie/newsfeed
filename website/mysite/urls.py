9929efdc29e4771039190e673ce88385143cfdc0
cookiezombie <34878417+cookiezombie@users.noreply.github.com>
2021-09-14 17:42:54 -0400

    Added news path

--- website/mysite/urls.py
+++ website/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('mysite/', views.index, name='index'),
+    path('news/', views.news),
 ]
